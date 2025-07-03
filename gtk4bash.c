0efa589cf1921a929a006aa87366075e2004c4e3
Monika Lobinger <monika.lobinger@gmx.de>
2025-07-03 10:11:37 +0200

    feat: Aktiviere close callback

--- gtk4bash.c
+++ gtk4bash.c
@@ -11,12 +11,9 @@
 
 #include <gtk/gtk.h>
 
-GtkBuilder *g_builder;
-GtkWidget *g_window;
-GtkApplication *g_app;
-
-void ok_button_clicked(GObject *object, gpointer user_data){
+void ok_button_clicked(GObject *object, GtkWindow *win){
     g_print("START ok_button_clicked\n");
+    gtk_window_destroy (win);
     g_print("END ok_button_clicked\n");
 }
 
@@ -29,16 +26,15 @@ static void app_activate(GtkApplication *app, gpointer user_data) {
 
 
     builder = gtk_builder_new_from_file (filename);
-    //g_builder = builder;
     dialog = GTK_WIDGET (gtk_builder_get_object (builder, main_object));
     gtk_window_set_application (GTK_WINDOW (dialog), GTK_APPLICATION (app));
-    //g_signal_connect (btn, "clicked", G_CALLBACK (ok_button_clicked), NULL);
-    //g_window = window;
-    //gtk_window_set_transient_for(GTK_WINDOW (dialog), GTK_WINDOW (appwin));
+
+
+    GtkWidget *widget = GTK_WIDGET(gtk_builder_get_object( builder, "ok_button"));
+    g_signal_connect (widget, "clicked", G_CALLBACK (ok_button_clicked), dialog);
     gtk_window_present (GTK_WINDOW (dialog));
 
-    //g_object_unref (appwin);
-    //g_object_unref (builder);
+    g_object_unref (builder);
     g_print("END app_activate\n");
 }
 
