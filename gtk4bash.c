9e511f0351fae9610198383cfe9996ab731cec97
Monika Lobinger <monika.lobinger@gmx.de>
2025-07-03 12:40:19 +0200

    feat: Verarbeite Options

--- gtk4bash.c
+++ gtk4bash.c
@@ -8,10 +8,15 @@
 #include <unistd.h>
 #include <libintl.h>
 
-struct _args {
+short VERBOSE = 0;
+
+typedef struct {
+    char *app_name;
     char *ui_file;
     char *dlg_id;
-};
+    char *pipe_out;
+    char *pipe_in;
+} _args;
 
 void on_close(GObject *object, gpointer user_data){
     g_print("START on_close\n");
@@ -25,14 +30,14 @@ void on_close(GObject *object, gpointer user_data){
 
 static void do_dialog(GtkApplication *app, gpointer *user_data) {
     g_print("START do_dialog\n");
-    struct _args *args;
+    _args *args;
     GtkBuilder *builder;
     GtkWidget *dialog; // GtkDialog deprecated with GTK4.10, use GtkWindow
     GtkWidget *btn_close;
     char *filename;
     char *main_object;
 
-    args =        (struct _args *)user_data;
+    args =        (_args *)user_data;
     filename =    args->ui_file;
     main_object = args->dlg_id;
 
@@ -54,27 +59,105 @@ static void app_activate(GtkApplication *app, gpointer user_data) {
     g_print("START app_activate\n");
     do_dialog(app, user_data);
     g_print("END app_activate\n");
-}
+  }
 static void app_open(GtkApplication *app, GFile ** files, gint n_files, gchar *hint, gpointer *user_data) {
     /* Dieser Funktion endet mit dem Anzeigen des Dialogs. 
      * Danach befinden wir uns in g_application_run */
     g_print("START app_open\n");
     do_dialog(app, user_data);
     g_print("END app_open\n");
-}
+  }
+static void help(){
+    char* appname="xxx";
+    printf("\
+    Aufruf:\n\
+    %s -f string [-m string][-v][-i string][-o string]\n\
+    -f DLGFILE\t\t Die Dialogdatei\n\
+    -m OBJECTNAME \t OBJECTNAME fürs main window. Vorgabe ist \"window1\".\n\
+    -v \t\t\t Verbose.\n\
+    -i INPIPE \t\t Benutze INPIPE anstatt standard input.\n\
+    -o OUTPIPE \t\t Benutze OUTPIPE anstatt standard output.\n"
+    , appname);
+    
+    exit(1);
+  }
+static void read_opts(_args *pargs, int *pargc, char*** pargv) {
+    char **argv;
+    int argc;
+    int opt;
+    int pos;
 
+    argc = *pargc;
+    argv = *pargv;
+
+    while((opt = getopt(argc, argv, ":vhf:m:i:o:")) != -1) { 
+        switch(opt) { 
+            case 'v': 
+                VERBOSE = 1;
+                if(VERBOSE) printf("-v\n"); 
+                break; 
+            case 'h': 
+                if(VERBOSE) printf("-h\n"); 
+                help();
+                break; 
+            case 'f': 
+                if(VERBOSE) printf("-%c: %s\n", opt, optarg);
+                if(optarg[0]=='-') printf("ACHTUNG: -%c: %s\n", opt, optarg);
+                pargs->ui_file  = optarg;
+                break; 
+            case 'm': 
+                if(VERBOSE) printf("-%c: %s\n", opt, optarg);
+                if(optarg[0]=='-') printf("ACHTUNG: -%c: %s\n", opt, optarg);
+                pargs->dlg_id  = optarg;
+                break; 
+            case 'i': 
+                if(VERBOSE) printf("-%c: %s\n", opt, optarg);
+                if(optarg[0]=='-') printf("ACHTUNG: -%c: %s\n", opt, optarg);
+                pargs->pipe_out  = optarg;
+                break; 
+            case 'o': 
+                if(VERBOSE) printf("-%c: %s\n", opt, optarg);
+                if(optarg[0]=='-') printf("ACHTUNG: -%c: %s\n", opt, optarg);
+                pargs->pipe_in  = optarg;
+                break; 
+            case ':': 
+                if(VERBOSE) printf("Die Option braucht einen Wert\n"); 
+                break; 
+            case '?': 
+                if(VERBOSE) printf("Option %c wird nicht unterstützt\n", optopt);
+                break; 
+        } 
+    } 
+    
+    for(pos=argc-optind-1; optind < argc; optind++){     
+        if(VERBOSE) printf("ARG: %s\n", argv[optind]); 
+        argv[pos++]=argv[optind];
+    }  
+    *pargc=pos;
+  }
 int main(int argc, char **argv) {
     g_print("START main\n");
     int stat;
+    int idx;
     GtkApplication *app;
     const char *app_id;
     GApplicationFlags app_flags;
-    struct _args args;
+    _args args;
 
     app_id =       "daheim.oskopia.gtk4bash";
     app_flags =    G_APPLICATION_HANDLES_OPEN;
-    args.ui_file = "dlg.ui";
-    args.dlg_id  = "win1";
+
+    args.app_name = argv[0];
+    args.ui_file  = NULL;
+    args.dlg_id   = "window1";
+    args.pipe_out = NULL;
+    args.pipe_in  = NULL;
+
+    read_opts(&args, &argc, &argv);
+    if(!args.ui_file) 
+        help();
+    if(VERBOSE) for(idx=0; idx < argc; idx++)printf("%i: %s\n", idx, argv[idx]);
+    if(VERBOSE) printf("UI-Datei: %s; TOP-WINDOW: %s\n", args.ui_file, args.dlg_id);
 
     app = gtk_application_new(app_id, app_flags);
     g_signal_connect(app, "activate", G_CALLBACK (app_activate), &args);
